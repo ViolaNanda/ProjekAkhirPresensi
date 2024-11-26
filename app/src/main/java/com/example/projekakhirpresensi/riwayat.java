074cfac90c86889ca1a84d6cc4d63ad07c220a8d
VIOLA NANDA <viola.nanda38@smk.belajar.id>
2024-11-26 11:27:53 +0700

    Initial commit

---
+++ app/src/main/java/com/example/projekakhirpresensi/riwayat.java
@@ -0,0 +1,21 @@
+package com.example.projekakhirpresensi;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.riwayat_activity);
+
+        // Back button click listener
+        Button backButton = findViewById(R.id.back_button);
+        backButton.setOnClickListener(view ->
+                Toast.makeText(MainActivity.this, "Back button clicked!", Toast.LENGTH_SHORT).show()
+        );
+    }
+}
